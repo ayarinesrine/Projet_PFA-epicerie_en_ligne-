7bab57af728c44ad004bf4a2af930e063ec65dfd
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-04 23:42:27 +0100

    up

---
+++ front/src/pages/Introduction.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import "./Introduction.css"
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div ></div>
+    )
+  }
+}
