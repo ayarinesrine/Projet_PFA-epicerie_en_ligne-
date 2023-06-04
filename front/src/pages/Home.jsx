d593ed708e45a57ee8fbcdae1132b423242a0bde
Ayari Nesrine <121802421+ayarinesrine@users.noreply.github.com>
2023-06-04 18:59:28 +0100

    up

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./Home.css";
 
 import FlipCard from "./FlipCard";
+import Slider from "./Slider";
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <Slider></Slider>
+       
         <FlipCard></FlipCard>
-    
       </div>
     );
   }
